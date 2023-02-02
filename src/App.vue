3494fd8538308111414e0a1965e8c3ebec8c2bd5
Pietro Giordano <giopi87@gmail.com>
2023-02-02 16:38:25 +0100

    avanzamento

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
       name: 'App',
+      components: {
+            AppHeader,
+            AppMain,
+            AppFooter
+      }
 }
-
 </script>
 
 <template>
-      prova
+      <AppHeader />
+
+      <AppMain />
+
+      <AppFooter />
 </template>
 
 <style lang="scss">
